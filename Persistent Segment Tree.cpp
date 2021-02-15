1026f3e9aad33fd3465165dfa06fbbde66e9c3a3
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-15 16:46:46 +0530

    Update Persistent Segment Tree.cpp

--- Persistent Segment Tree.cpp
+++ Persistent Segment Tree.cpp
@@ -1,52 +1,84 @@
-// Declaration : node* root = new node(NULL, NULL, 0) ;
- 
-struct node{
-	int val ; 
-	node *left, *right ;
-	node(node* l, node* r, int v) : left(l), right(r), val(v) {} 
+// Declaration : PST<int> P(size) ; 
+// Building the Segment Tree - P.build(a), 
+//			a must be a vector and 1-indexed
+// Update : P.update(k, index, val), update the value 
+//			at index to val in the kth version
+// Query : P.query(k, a, b), find the range sum [a,b] 
+//			in the kth version
+// Create a copy of kth version : P.newVersion(k) 
+
+template<typename T>
+struct PST { 
+    struct node{
+        T val ; 
+        node *left, *right ;
+        node(node* l, node* r, T v) : left(l), right(r), val(v) {} 
+    };
+     
+    int n;
+    node* root;  
+    vector<node*> version; // stores the root node of each version
+     
+    PST(int _n) { 
+        n = _n ; 
+        root = new node(NULL, NULL, 0); 
+        version.push_back(root) ;
+    } 
+
+    // 1 based indexing
+    node* getVersion(int k) { return version[k-1]; }
+
+    node* getNewNode(int k) {
+        node* temp = new node(getVersion(k)->left, getVersion(k)->right, getVersion(k)->val) ; 
+        return temp; 
+    }
+
+    void newVersion(int k) {
+        node* new_node = getNewNode(k) ; 
+        version.push_back(new_node) ; 
+    }
+
+    void _build(node* n, int start, int end, vector<T> &a) {
+        if(start == end ) {
+            n->val = a[start]; 
+            return ; 
+        }
+        int mid = (start+end)/2 ;
+        n->left = new node(NULL, NULL, 0); 
+        n->right = new node(NULL, NULL, 0); 
+        _build(n->left, start, mid, a); 
+        _build(n->right, mid+1, end, a); 
+        n->val = n->left->val + n->right->val ; 
+    }
+	
+    void _update(node* n, int start, int end, int idx, T v) {
+        if(idx > end or idx < start or start > end) return ; 
+        if(start == end) {
+            n->val = v; 
+            return ; 
+        }
+        int mid = (start + end)/2 ;
+        if(idx <= mid) {
+            node* new_node = new node(n->left->left, n->left->right, 0); 
+            n->left = new_node; 
+            _update(n->left, start, mid, idx, v) ;
+        }
+        else {  
+            node* new_node = new node(n->right->left, n->right->right, 0); 
+            n->right = new_node; 
+            _update(n->right, mid+1, end, idx, v); 
+        }
+        n->val = n->left->val + n->right->val ; 
+    }
+	
+    int _query(node* n,int start, int end, int l, int r) {
+        if(start > end or start > r or end < l) return 0 ; 
+        if(start >= l and end <= r) return n->val ; 
+        int mid = (start + end) / 2 ;
+        return _query(n->left, start, mid, l, r) + _query(n->right, mid+1, end, l, r); 
+    }
+
+    void build(vector<T> &a) { _build(root, 1, n, a) ; }
+    void update(int k, int idx, T val) { _update(getVersion(k), 1, n, idx, val); } 
+    int query(int k, int l, int r) { return _query(getVersion(k), 1, n, l, r) ; }
 };
- 
-const int N = 2e5 + 5; 
-int a[N]; 
-vector<node*> version; // stores the root node of each version
- 
-void build(node* n, int start, int end) {
-	if(start == end ) {
-		n->val = a[start]; 
-		return ; 
-	}
-	int mid = (start+end)/2 ;
-	n->left = new node(NULL, NULL, 0); 
-	n->right = new node(NULL, NULL, 0); 
-	build(n->left, start, mid); 
-	build(n->right, mid+1, end); 
-	n->val = n->left->val + n->right->val ; 
-}
- 
-void update(node* n, int start, int end, int idx, int v) {
-	if(idx > end or idx < start or start > end) return ; 
-	if(start == end) {
-		n->val = v; 
-		return ; 
-	}
-	int mid = (start + end)/2 ;
-	if(idx <= mid) {
-		node* new_node = new node(n->left->left, n->left->right, 0); 
-		n->left = new_node; 
-		update(n->left, start, mid, idx, v) ;
-	}
-	else {	
-		node* new_node = new node(n->right->left, n->right->right, 0); 
-		n->right = new_node; 
-		update(n->right, mid+1, end, idx, v); 
-	}
-	n->val = n->left->val + n->right->val ; 
-}
- 
-int query(node* n,int start, int end, int l, int r) {
-	if(start > end or start > r or end < l) return 0 ; 
-	if(start >= l and end <= r) return n->val ; 
-	int mid = (start + end) / 2 ;
-	return query(n->left, start, mid, l, r) + query(n->right, mid+1, end, l, r); 
-}
- 
