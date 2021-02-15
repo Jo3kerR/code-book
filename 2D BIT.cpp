43a624394f8ac4cd02364c0c6e5379e1d50c4b8e
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-15 09:38:29 +0530

    Update 2D BIT.cpp

--- 2D BIT.cpp
+++ 2D BIT.cpp
@@ -18,12 +18,17 @@ struct BIT_2D {
         }
     }
 
-    T query(int x,int y) {
+    T _query(int x,int y) {
         T sum = 0 ; 
         for(int i = x ; i > 0 ; i -= i&-i) {
             for(int j = y ; j > 0 ; j -= j&-j) 
                 sum += bit[i][j]; 
         }
         return sum ;
+    }
+
+    T query(int x1, int y1, int x2, int y2) {
+        return _query(x2, y2) - _query(x1, y2) -
+            _query(x2, y1) + _query(x1, y1) ; 
     }
 };
