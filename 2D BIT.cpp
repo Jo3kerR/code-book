53bbe5a2f34621e53c91ef388be044a69d299037
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-15 09:31:59 +0530

    Update 2D BIT.cpp

--- 2D BIT.cpp
+++ 2D BIT.cpp
@@ -1,4 +1,3 @@
-
 template<typename T>
 struct BIT_2D {
 
@@ -11,7 +10,7 @@ struct BIT_2D {
         bit.resize(X, vector<int>(Y)) ; 
     }
 
-    void update(int x,int y,int val) {
+    void update(int x,int y, T val) {
         for(int i = x ; i <= X ; i += i&-i) {
             for(int j = y ; j <= Y ; j += j&-j) {
                 bit[i][j] += val ; 
@@ -19,8 +18,8 @@ struct BIT_2D {
         }
     }
 
-    int query(int x,int y) {
-        int sum = 0 ; 
+    T query(int x,int y) {
+        T sum = 0 ; 
         for(int i = x ; i > 0 ; i -= i&-i) {
             for(int j = y ; j > 0 ; j -= j&-j) 
                 sum += bit[i][j]; 
