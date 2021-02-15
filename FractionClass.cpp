5cc18d10c5534107f25abf384540f221e034fc7a
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-15 15:40:27 +0530

    Update FractionClass.cpp

--- FractionClass.cpp
+++ FractionClass.cpp
@@ -1,33 +1,17 @@
-
 template<typename T>
 struct frac{
     T num ,den ;
-
     frac(T x,T y) {
         num = x, den = y ; 
         T g = __gcd(x,y) ;
         num /= g , den /= g ;
     }
 
-    bool operator > (const frac &b) {
-        return num * b.den > b.num * den ;
-    }
-
-    bool operator < (const frac &b) {
-        return num * b.den < b.num * den ;
-    }
-
-    bool operator <= (const frac &b) {
-        return num * b.den <= b.num * den ;
-    }
-
-    bool operator >= (const frac &b) {
-        return num * b.den >= b.num * den ;
-    }
-
-    bool operator == (const frac &b) {
-        return num * b.den == b.num * den ;
-    }
+    bool operator > (const frac &b) { return num * b.den > b.num * den ;}
+    bool operator < (const frac &b) { return num * b.den < b.num * den ;}
+    bool operator <= (const frac &b) { return num * b.den <= b.num * den ;}
+    bool operator >= (const frac &b) { return num * b.den >= b.num * den ;}
+    bool operator == (const frac &b) { return num * b.den == b.num * den ;}
 
     frac operator - (const frac &b) {
         frac f((num * b.den - b.num * den) , den * b.den); 
