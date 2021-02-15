0851e13928489a9e5965e2397b8e921597593c3a
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-15 08:19:23 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,38 +1,25 @@
-#include<bits/stdc++.h>
-using namespace std ;
 
 struct DSU{  
-    int n;
-    vector<int> par, rnk ;
-    
-    DSU(int _n) {
-        n = _n ; 
-        par.resize(n) ; 
-        rnk.resize(n) ; 
-    }
-     
-    int find_set(int a) {
-        return (a == par[a] ? a : par[a] = find_set(par[a])) ; 
-    }
-     
-    void union_set(int a, int b) {
-        a = find_set(a) ; 
-        b = find_set(b) ; 
-        if(a != b) {
-            if(rnk[a] < rnk[b]) swap(a, b) ; 
-            par[b] = a ; 
-            if(rnk[a] == rnk[b]) ++rnk[a] ; 
-        }
-    }
+	int n;
+	vector<int> par, rnk ;
+	
+	DSU(int _n) {
+		n = _n ; 
+		par.resize(n) ; 
+		rnk.resize(n) ; 
+	}
+	 
+	int find_set(int a) {
+	    return (a == par[a] ? a : par[a] = find_set(par[a])) ; 
+	}
+	 
+	void union_set(int a, int b) {
+	    a = find_set(a) ; 
+	    b = find_set(b) ; 
+	    if(a != b) {
+	        if(rnk[a] < rnk[b]) swap(a, b) ; 
+	        par[b] = a ; 
+	        if(rnk[a] == rnk[b]) ++rnk[a] ; 
+	    }
+	}
 };
-
-signed main() {
-
-    ios::sync_with_stdio(0) ; 
-    cin.tie(0) ; 
-
-    DSU d(10) ;     
-
-    return 0 ; 
-
-}
