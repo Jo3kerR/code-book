c7f31a33401c6e84fe372f1edfbe3affd302aa7a
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-15 18:26:33 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,25 +1,25 @@
-
 struct DSU{  
-	int n;
-	vector<int> par, rnk ;
-	
-	DSU(int _n) {
-		n = _n ; 
-		par.resize(n) ; 
-		rnk.resize(n) ; 
-	}
-	 
-	int find_set(int a) {
-	    return (a == par[a] ? a : par[a] = find_set(par[a])) ; 
-	}
-	 
-	void union_set(int a, int b) {
-	    a = find_set(a) ; 
-	    b = find_set(b) ; 
-	    if(a != b) {
-	        if(rnk[a] < rnk[b]) swap(a, b) ; 
-	        par[b] = a ; 
-	        if(rnk[a] == rnk[b]) ++rnk[a] ; 
-	    }
-	}
+        int n;
+        vector<int> par, rnk ;
+        
+        DSU(int _n) {
+            n = _n + 5 ; 
+            par.resize(n) ; 
+            rnk.resize(n) ;
+            for(int i = 1 ; i <= n ; ++i) par[i] = i ; 
+        }
+         
+        int find_set(int a) {
+            return (a == par[a] ? a : par[a] = find_set(par[a])) ; 
+        }
+         
+        void union_set(int a, int b) {
+            a = find_set(a) ; 
+            b = find_set(b) ; 
+            if(a != b) {
+                if(rnk[a] < rnk[b]) swap(a, b) ; 
+                par[b] = a ; 
+                if(rnk[a] == rnk[b]) ++rnk[a] ; 
+            }
+        }
 };
