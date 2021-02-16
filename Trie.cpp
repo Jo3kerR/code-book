811670b832af1795556993b7c2f5df1e64e9cec6
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-16 10:37:36 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,30 +1,32 @@
 struct Trie{
-	struct node {
-		int cnt = 0 ;
-		node* child[26] ; 
-	};
+    struct node {
+        int cnt = 0 ;
+        node* child[2] ; 
+    }; 
+    node* getNewNode() {
+        node* t = new node() ;
+        for(int i = 0 ; i < 2 ; ++i) t->child[i] = NULL ; 
+        return t; 
+    }
+    
+    node* root ;
+    Trie() {
+        root = getNewNode(); 
+    }
  
-	node* getNewNode() {
-		node* t = new node() ;
-		for(int i = 0 ; i < 26 ; ++i) t->child[i] = NULL ; 
-		return t; 
-	}
- 
-	node* root ; 
- 
-	void add(string &s) {
-		node* ex = root ; 
-		for(auto i : s) {
-			if(root->child[i-'a'] == NULL) {
-				root->child[i-'a'] = getNewNode() ;
-			}
-			root->cnt ++ ; 
-			root = root->child[i-'a'] ;
-		}
-		root = ex; 
-	}
+    void add(string &s) {
+        node* ex = root ; 
+        for(auto i : s) {
+            if(root->child[i-'('] == NULL) {
+                root->child[i-'('] = getNewNode() ;
+            }
+            root = root->child[i-'('] ;
+        }
+        root -> cnt ++ ; 
+        root = ex; 
+    }
 
-	void dfs(node* r) {
-		complete the function
-	}
+    int dfs(string &s) {
+        complete the function 
+    }
 };
