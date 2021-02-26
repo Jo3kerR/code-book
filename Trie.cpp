21495cd76d19fdb2ce0b1b70bcea2b8190f0ea73
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-02-26 18:02:09 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,10 +1,12 @@
 struct Trie{
     struct node {
         int cnt = 0 ;
+        check structure
         node* child[2] ; 
     }; 
     node* getNewNode() {
         node* t = new node() ;
+        check init
         for(int i = 0 ; i < 2 ; ++i) t->child[i] = NULL ; 
         return t; 
     }
@@ -15,6 +17,7 @@ struct Trie{
     }
  
     void add(string &s) {
+        check add 
         node* ex = root ; 
         for(auto i : s) {
             if(root->child[i-'('] == NULL) {
