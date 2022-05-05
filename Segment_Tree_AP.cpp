28d0dbfb44a69b767f99c99071e19fd52314b997
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2022-05-05 10:41:04 +0530

    add first term and common difference of AP

--- Segment_Tree_AP.cpp
+++ Segment_Tree_AP.cpp
@@ -1,4 +1,6 @@
-// add 1,2,3,...,j-i+1 over range [i,j] 
+// add a,a + d,a + 2d,...,a + (j - i)d over range [i,j] 
+// https://codeforces.com/contest/1661/submission/153189277
+
 struct Node {
 	int sum, A, D;
 	Node() {
@@ -35,25 +37,27 @@ void prop(int node,int start, int end) {
 		tree[node].reset() ;
 	}
 }
-void update(int l, int r, int node=1, int start = 1, int end = n) {
-	prop(node,start,end) ;
-	if(start > end or start > r or end < l) return ; 
-	if(start >= l and end <= r ) {
-		int s = (end - start + 1) ; 
-		tree[node].sum += s * (2 * (start-l+1) + (s-1)) / 2 ;
-		if(start != end ) {
-			tree[2*node].A += (start-l+1);
-			tree[2*node].D ++ ; 
-			tree[2*node+1].A += (start-l+1) + (mid+1-start); 
-			tree[2*node+1].D ++ ; 
-		}
-		return ; 
-	}
-	int mid = (start + end) / 2; 
-	update(l,r,2*node,start,mid);
-	update(l,r,2*node+1,mid+1,end) ;
-	tree[node].sum = tree[2*node].sum + tree[2*node+1].sum ;
+
+void update(int l, int r, int node, int start, int end, int first_term, int cmn_diff) {
+    prop(node,start,end) ;
+    if(start > end or start > r or end < l) return ;  
+    int mid = (start + end) / 2; 
+    if(start >= l and end <= r) {
+        int s = (end - start + 1) ; 
+        tree[node].sum += s * (2 * (first_term + (start-l) * cmn_diff) + (s-1)) / 2 ;
+        if(start != end ) {
+            tree[2*node].A += (first_term + (start-l) * cmn_diff);
+            tree[2*node].D += cmn_diff ; 
+            tree[2*node+1].A += (first_term + (mid + 1-l) * cmn_diff); 
+            tree[2*node+1].D += cmn_diff ; 
+        }
+        return ; 
+    }
+    update(l,r,2*node,start,mid, first_term, cmn_diff);
+    update(l,r,2*node+1,mid+1,end, first_term, cmn_diff); 
+    tree[node].sum = tree[2*node].sum + tree[2*node+1].sum;
 }
+ 
 int query(int l,int r,int node=1,int start=1, int end=n) {
 	prop(node,start,end) ;
 	if(start > end or start > r or end < l) return 0 ;
