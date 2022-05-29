bd6ac988fa1e9ba675323b60faed03751943ed1f
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2022-05-29 11:04:11 +0530

    Create Mo's algo.cpp

---
+++ Mo's algo.cpp
@@ -0,0 +1,49 @@
+const int BLOCK_SIZE = 300;
+
+struct Query {
+    int l, r, idx;
+    bool operator<(Query &other) const {
+        if (l / BLOCK_SIZE != other.l / BLOCK_SIZE) return make_pair(l, r) < make_pair(other.l, other.r);
+        return (l / BLOCK_SIZE & 1) ? (r < other.r) : (r > other.r);
+    }
+};
+
+void remove(int idx) {
+
+}
+
+void add(int idx) {
+
+}
+
+int get_answer() {
+
+}  
+
+vector<int> mo_s_algorithm(vector<Query> queries) {
+    vector<int> answers(queries.size());
+    sort(queries.begin(), queries.end());
+
+    int cur_l = 0;
+    int cur_r = -1;
+    for (Query q : queries) {
+        while (cur_l > q.l) {
+            cur_l--;
+            add(cur_l);
+        }
+        while (cur_r < q.r) {
+            cur_r++;
+            add(cur_r);
+        }
+        while (cur_l < q.l) {
+            remove(cur_l);
+            cur_l++;
+        }
+        while (cur_r > q.r) {
+            remove(cur_r);
+            cur_r--;
+        }
+        answers[q.idx] = get_answer();
+    }
+    return answers;
+}
